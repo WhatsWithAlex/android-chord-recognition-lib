52cb5d284e0aef16aac8bb1bde997d132a365997
WhatsWithAlex <joktx@mail.ru>
2023-03-28 02:56:28 +0300

    sample volume detector app implemented

--- app/src/main/java/com/example/chord_recognition_app/MainActivity.kt
+++ app/src/main/java/com/example/chord_recognition_app/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.chord_recognition_app
 
+import android.Manifest.permission.RECORD_AUDIO
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.ToggleButton
+import io.reactivex.disposables.CompositeDisposable
+import com.example.chord_recognition_lib.ChordRecognizer
 
 class MainActivity : AppCompatActivity() {
+    val TAG = javaClass.simpleName
+
+    val disposable: CompositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestAudio()
+
+        val toggleButton = findViewById<ToggleButton>(R.id.toggleButton)
+
+        toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                start()
+            } else {
+                stop()
+            }
+        }
+    }
+
+    private fun start() {
+        val chordLabelText = findViewById<TextView>(R.id.chordLabel)
+        val chordStream = ChordRecognizer(44110, 4096, 2048).chordStream()
+
+        disposable.add(chordStream.subscribe({ chordLabel ->
+            Log.d(TAG, chordLabel)
+            chordLabelText.text = chordLabel
+        }, {e -> e.message?.let { Log.e(TAG, it) }}))
+    }
+
+    private fun stop() {
+        val chordLabelText = findViewById<TextView>(R.id.chordLabel)
+
+        disposable.clear()
+        chordLabelText.text = ""
+    }
+
+    private fun requestAudio(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(RECORD_AUDIO) != PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(RECORD_AUDIO), 1337)
+            return false
+        }
+
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (grantResults[0] == PERMISSION_GRANTED)
+            start()
     }
 }
--- chord-recognition-lib/src/main/java/com/example/chord_recognition_lib/ChordRecognizer.kt
+++ chord-recognition-lib/src/main/java/com/example/chord_recognition_lib/ChordRecognizer.kt
@@ -1,4 +1,44 @@
 package com.example.chord_recognition_lib
 
-class ChordRecognizer {
+import android.util.Log
+import androidx.annotation.NonUiContext
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import com.paramsen.noise.Noise
+import com.example.chord_recognition_lib.AudioSource
+
+class ChordRecognizer(
+    private val sampleFrequency: Int,
+    private val windowLength: Int,
+    private val hopLength: Int,
+) {
+    val TAG = javaClass.simpleName
+
+    private val noise = Noise.real(windowLength)
+    private val chordStream: Flowable<String> = AudioSource().stream()
+        .observeOn(Schedulers.newThread())
+        .map { calculateDFT(it) }
+        .map { calculateChromagram(it) }
+        .map { estimateChord(it) }
+
+    private fun calculateDFT(signalFrame: FloatArray): FloatArray {
+        return noise.fft(signalFrame, FloatArray(4096 + 2))
+    }
+
+    private fun calculateChromagram(fft: FloatArray): Float {
+        var sum = 0f
+        for (i in fft.indices) {
+            sum += fft[i] * fft[i]
+        }
+        return (sum / (fft.size * fft.size))
+    }
+
+    private fun estimateChord(k: Float): String {
+        return k.toInt().toString()
+    }
+
+    fun chordStream(): Flowable<String> {
+        return chordStream
+    }
 }
